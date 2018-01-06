6692c5c4ce68f7c2cf2ec6c5fe116b1206fd3692
Matthias Wagener <m.wagener@netconomy.net>
2018-01-06 19:19:08 +0100

    patch route added and needed libs

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 // libs
+const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
@@ -66,9 +67,30 @@ app.delete('/todos/:id', (req, res) =>  {
   }).catch((e) => {
     res.status(400).send();
   });
-    // success
-    // errror -> 400 -> empty body
 });
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send({});
+  }
+  if(_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if(!todo) {
+      return res.send(400).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    return res.status(400).send();
+  });
+});
+
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000.');
